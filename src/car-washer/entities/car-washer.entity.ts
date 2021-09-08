aacd68874a6120f268b8e17b3a174e6b93c2c4a7
davi-OD <davidodie51@gmail.com>
2021-09-08 21:51:13 +0300

    Updated file car-washer

--- src/car-washer/entities/car-washer.entity.ts
+++ src/car-washer/entities/car-washer.entity.ts
@@ -1,10 +1,13 @@
 /* eslint-disable prettier/prettier */
 import { Car } from "src/car/entities/car.entity";
 import { Washer } from "src/washer/entities/washer.entity";
-import { Entity, ManyToOne } from "typeorm";
+import { Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity('carWasher')
 export class CarWasher {
+    @PrimaryGeneratedColumn()
+    id: number;
+
     @ManyToOne(() => Washer, washer => washer.carW)
     washer: Washer;
 
