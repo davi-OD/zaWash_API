7235469bcd033c3869d9e156edf878b0c39819ae
davi-OD <davidodie51@gmail.com>
2021-09-08 21:45:09 +0300

    Updated files

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -2,11 +2,11 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const config: TypeOrmModuleOptions = {
     type: 'postgres',
-    username: '',
-    password: '',
+    username: 'tqhyfkjkoaagqc',
+    password: 'f84d72e69be05c4c11432dd8ee410ae3f4a9bf6f05df37d27e55408c82143769',
     port: 5432,
-    host: '',
-    database: '',
+    host: 'ec2-52-0-93-3.compute-1.amazonaws.com',
+    database: 'dd45urulqhv2tc',
     synchronize: true,
     entities: ['dist/**/*.entity{.ts,.js}'],
     ssl:{rejectUnauthorized:false},
